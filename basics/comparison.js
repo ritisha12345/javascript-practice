864954bab3bc74ba8f51736499a88d5c9a8e05cf
Ritisha Bobde <147045463+ritisha12345@users.noreply.github.com>
2024-07-14 05:11:42 +0000

    COMPARISONS

---
+++ basics/comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 >1);
+// console.log(2 >=1);
+// console.log(2 <1);
+// console.log(2 ==1);
+// console.log(2!=1);
+
+// console.log("2" >1); always compare same datatypes
+// console.log("02" >1); predictable outputs asre not gained when we compare two different datatypes 
+
+
+
+// **immp**
+// console.log(null >0); false output
+// console.log(null >=0); false output
+// console.log(null==0);  true
+// the reason is that an equality check == and comparisons ><>=<= work differently.
+//Comparisons convert null to a number, treating it as 0.
+//That's why null>=0 is true and null>0 is false
+
+
+// === 
+console.log("2" === 2);
+
+
+
