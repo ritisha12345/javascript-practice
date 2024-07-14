a27767b26278ed7c59a06cc65a2446a4b5a4c738
Ritisha Bobde <147045463+ritisha12345@users.noreply.github.com>
2024-07-14 04:24:13 +0000

    conversion of datatypes

---
+++ basics/conversionoperations.js
@@ -0,0 +1,25 @@
+let score = 33
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//notes
+//"33" = 33
+//"33abc" = NaN(not a number)
+//true = 1 and false = 0
+
+let isloggedin = 1
+
+let booleanisloggedin = Boolean(isloggedin)
+//console.log(booleanisloggedin);
+
+
+//1= true ; 0 =false 
+//""" = fasle
+// "hitesh" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
