74b7adcddc6f81e94608cc0f6498ecd80924f3c8
Ritisha Bobde <147045463+ritisha12345@users.noreply.github.com>
2024-07-14 04:51:08 +0000

    OPERATIONS

--- basics/conversionoperations.js
+++ basics/conversionoperations.js
@@ -21,5 +21,46 @@ let booleanisloggedin = Boolean(isloggedin)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********OPERATIONS******************
+
+
+
+let value = 3
+let negvalue =-value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 ="hello"
+let str2 =" ritu"
+
+let str3 = str1 +str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(1+(3*4)%5);
+
+
+// console.log( +true);
+// console.log(+"");
+
+let num1,num2,num3
+num1 = num2 =num3 =2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
