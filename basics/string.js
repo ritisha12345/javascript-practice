beab817b21cdee2aa5343181de6dfcaf7af620ef
Ritisha Bobde <147045463+ritisha12345@users.noreply.github.com>
2024-09-17 09:36:46 +0000

    strings

---
+++ basics/string.js
@@ -0,0 +1,18 @@
+const name = "ritu"
+const repoCount = 50
+//console.log(name + repoCount); *outdated syntax or way 
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);// more preferred way
+const GameName = new String('ritu')//another way to declare a string
+// console.log(GameName[0]);//accessing the keyvalue pair
+// console.log(GameName.length);
+
+
+
+console.log(GameName.charAt(2));//kis position par konsa character hai 
+console.log(GameName.indexOf("t"));//character konse index par hai
+
+
+
+
+
+
