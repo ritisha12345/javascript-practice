cb263f3fd54fce5a3b0ac48a6b78d0ad9f4eb568
Ritisha Bobde <147045463+ritisha12345@users.noreply.github.com>
2024-09-21 16:18:26 +0000

    strings

--- basics/string.js
+++ basics/string.js
@@ -2,7 +2,7 @@ const name = "ritu"
 const repoCount = 50
 //console.log(name + repoCount); *outdated syntax or way 
 console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);// more preferred way
-const GameName = new String('ritu')//another way to declare a string
+const GameName = new String('ritu-i am-girl')//another way to declare a string
 // console.log(GameName[0]);//accessing the keyvalue pair
 // console.log(GameName.length);
 
@@ -10,6 +10,20 @@ const GameName = new String('ritu')//another way to declare a string
 
 console.log(GameName.charAt(2));//kis position par konsa character hai 
 console.log(GameName.indexOf("t"));//character konse index par hai
+
+
+const newString = GameName.substring(0, 4)
+console.log(newString);
+
+// const anotherString = GameName
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'));
+console.log(url.includes("ritu"));
+console.log(GameName.split('-'));
+
+
+
 
 
 
